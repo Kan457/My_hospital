30713d941803abb31a5dc3c634ab3de52ad5c8da
Каневская Елизавета Юрьевна <lizok.kanev@mail.ru>
2025-10-21 18:00:06 +0300

    Class Patient.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def __init__(self, name : str , addres : str , chief_medical : str , mail : str,
 def hocpital_info(self):
   return f"Название: {self.name}\nАдрес: {self.addres}\nГлавный врач: {self.chief_medical}\nПочта: {self.mail}"
   
-def withdrawal_of_patients(self):#выво инфы о пациентах
+def withdrawal_of_patients(self,patient_dict : dict):#вывод инфы о пациентах
   count = 0
   for key in self.patient_dict.keys():
     count+=1
-    print(f"{count} - {key}")
+    return (f"{count} - {key}")
     
 def to_dict(self):
     return {
@@ -26,7 +26,25 @@ def to_dict(self):
     }
 
 #--------------Пациент-----------
-  
-  
+class Patient:
+    def __init__(self, id : int , name : str, gender : str , age : str):
+        self.id = id
+        self.name = name
+        self.gender = gender
+        self.age = age
+        
+    def checking_the_attachment(patient_dict: dict, value) -> bool:#проверка прикрепления
+        return value in patient_dict.values()
+        
+    def to_dict(self):
+        return {
+          'ID': self.id,
+          'ФИО' : self.name,
+          'Пол': self.gender,
+          'Возраст': self.age,
+        }
+#--------------Прикрепление-----------
+class Attachment:
+
 
     
