e6451e862ecc3c650df333b7b15875d9c0fbca69
Каневская Елизавета Юрьевна <lizok.kanev@mail.ru>
2025-10-25 18:47:08 +0300

    patient
    Create class patient

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import random
 
+Hospital_patient = [] # Список пациентов в больнице
 class Generator_ID:
     def get_id():
         pass
@@ -13,15 +14,29 @@ class Polis(Generator_ID):
 
 
 #--------------Пациент-----------
+# Пациент - чел, у которого есть полис 
 class Patient:
-    def __init__(self, name : str, gender : str , age : str):
+    def __init__(self, polic : int , name : str, gender : str , age : str):
+        self.polic = polic
         self.name = name
         self.gender = gender
         self.age = age
         
+    def checking_policy(self, polic : int , object : "Patient"):
+        if polic=="":
+            print("Полис отсутсвует! Нужно создать ?")
+            request = input("Да/Нет")
+            if request=="Да":
+                #create_policy - создание полиса
+                ...
+            else:
+                s = f"Объект {self.object} удален :(((( "
+                del object
+                return s
+            
     def to_dict(self):
         return {
-          'ID': self.id,
+          'Policy': self.polic,
           'ФИО' : self.name,
           'Пол': self.gender,
           'Возраст': self.age,
@@ -29,7 +44,8 @@ class Patient:
     
 #--------------Больница-----------
 class Hospital:
-    def __init__(self, name : str , addres : str , chief_medical : str , mail : str, patient_dict : dict[Patient, Polis]):
+    def __init__(self, name : str , addres : str , chief_medical : str , mail : str, 
+                 patient_dict : dict[Patient, Polis]):
         self.name = name
         self.addres = addres
         self.chief_medical = chief_medical
@@ -38,12 +54,6 @@ class Hospital:
     
     def hocpital_info(self):
         return f"Название: {self.name}\nАдрес: {self.addres}\nГлавный врач: {self.chief_medical}\nПочта: {self.mail}"
-    
-    def withdrawal_of_patients(self):#вывод инфы о пациентах
-        count = 0
-        for key in self.patient_dict.keys():
-            count+=1
-            print (f"{count} - {key}")
         
     def to_dict(self):
         return {
@@ -53,42 +63,9 @@ class Hospital:
         'Mail.ru': self.mail,
         }
     
+#--------------------------------
+    
 hospital1 = Hospital("Bolnica", "addres0", "afasf", "ilyaer@dgf", {Patient("sdg", "m", 18) : Polis.get_id(),
                                                                    Patient("sdgd", "m", 19) : Polis.get_id(),
                                                                    Patient("sdget", "m", 20) : Polis.get_id()})
-    
-#--------------Прикрепление----------- пациент - прикрепление(агрегация)
-class Attachment:
-    def __init__(self, date, status, patient: Patient):
-        self.date = date 
-        self.status = status
-        self.patient = patient 
-        
-    def checking_the_attachment(self, patient_dict: dict, value : str):#проверка прикрепления
-        if value in patient_dict.values():
-            print('Пациент прикреплен')
-            return True
-        else:
-            patient_dict[self.patient] = Polis.get_id()
-
-
-
-    '''
-                new_id = ''.join(random.choice('0123456789') for _ in range(4))
-            
-            # Если передан объект пациента, обновляем его ID
-            if patient_info and hasattr(patient_info, 'id'):
-                patient_info.id = new_id
-                print(f'Сгенерирован новый ID для пациента: {new_id}')
-            
-            # Добавляем в словарь patient_dict
-            patient_dict[patient_name] = new_id
-            print(f'Пациент {patient_name} прикреплен с ID: {new_id}')
-            return False
-    '''
-            
-    def to_dict(self):
-        return {
-          'ID': self.date,
-          'Статус' : self.status,
-        }
+/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
